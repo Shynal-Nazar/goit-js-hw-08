a9bcd8df3bb161344e7af7e7280eb59804e6f526
Shynal-Nazar <87614426+Shynal-Nazar@users.noreply.github.com>
2021-12-13 19:15:22 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galaryCreate = createGalleryItem(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galaryCreate);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"          
+          alt="${description}"
+        />
+      </a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+  scrollZoomFactor: 0.5,
+  captionPosition: 'bottom',
+});
