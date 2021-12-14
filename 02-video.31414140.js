63860e9dbe673661aa3f66f3182b298e34bf6832
Shynal-Nazar <87614426+Shynal-Nazar@users.noreply.github.com>
2021-12-14 19:46:36 +0000

    Deploying to gh-pages from @ Shynal-Nazar/goit-js-hw-08@88ea4a6438525507d11f417fe2f8b74123e8ffca 🚀

--- 02-video.31414140.js
+++ 02-video.31414140.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=r(require("@vimeo/player")),t=r(require("lodash.throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("iframe"),u=new Vimeo.Player(o),a=localStorage.getItem("videoplayerCurrentTime");function i(e){localStorage.setItem("videoplayerCurrentTime",e.seconds)}a>=0&&u.setCurrentTime(a),u.on("timeupdate",(0,t.default)(i,1e3));
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.6c032b9c.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.31414140.js.map
