278b84f69e08b65a3b1f85480f525e99d58ba4c4
Shynal-Nazar <87614426+Shynal-Nazar@users.noreply.github.com>
2021-12-14 19:53:26 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const timeData = localStorage.getItem('videoplayerCurrentTime');
+
+if (timeData >= 0) {
+  player.setCurrentTime(timeData);
+}
+
+player.on('timeupdate', throttle(savedTime, 1000));
+function savedTime(data) {
+  localStorage.setItem('videoplayerCurrentTime', data.seconds);
+}
